7d3a158f2b0f0b50d40692f400f2434e8794bd29
Mitja Rislakki <57709659+mitjarislakki@users.noreply.github.com>
2024-03-25 14:22:44 +0200

    Add async 2 player support

--- src/patterns.cpp
+++ src/patterns.cpp
@@ -4,7 +4,6 @@
 #include <Wire.h>
 #include <hd44780.h>                       // main hd44780 header
 #include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander i/o class header
-#include <LinkedList.h>
 
 //Näyttöjen asetukset:
 hd44780_I2Cexp lcd1;
@@ -18,48 +17,71 @@ const int LCD_ROWS = 2;
 #define COLOR_ORDER GRB
 #define NUM_LEDS 31
 CRGB leds[NUM_LEDS];
+
 /*
 Input Matrix
 
 kytke diodit niin että virta menee row → col:
 row → napit → IN4148 diodit → col + 10k pulldown
 */
-//matriisin asetukset:
+
+// Matrix row/col pins
 const int row[] = {8,9,10,11};
 const int col[] = {2,3,4,5};
 const int size = 4;
 int keys[size*size];
 
+// Number of LEDs
 const int numLeds = 31;
 
-//pelaajien asetukset:
+// Player LED numbers
 const int LedP1[] = {0,1,2,3,4,5,6};
 const int LedP2[] = {7,8,9,10,11,12,13};
 
+// Player matrix pins
 const int KeysP1[] = {15,14,13,12,11,10,9};
 const int KeysP2[] = {7,6,5,4,3,2,1};
 
+// Player scores
 int scoreP1 = 0;
 int scoreP2 = 0;
 
 // patterns
 #define PATTERN_LENGTH 100
 int pattern[PATTERN_LENGTH];
+
+#define PATTERN_INTERVAL 500
+
+bool p1Pattern;
+bool p2Pattern;
+bool p1PStart = true;
+bool p2PStart = true;
+
+// player pattern indexes
+int indexP1P = 0;
+int indexP2P = 0;
+unsigned long prevTimeP1P = 0;
+unsigned long prevTimeP2P = 0;
+
+// player pattern variables
+
 int p1Cur = 0;
 int p2Cur = 0;
 int p1Max = 1;
 int p2Max = 1;
-bool p1Pattern = true;
-bool p2Pattern = true;
 
 
 // put function declarations here:
+
 void updateKeys();
 void updateScreens();
-void showPattern(int);
+void showPatterns(unsigned long);
 
 
 void setup() { 
+    p1Pattern = true;
+    p2Pattern = true;
+
     randomSeed(analogRead(0)); //randomise seed to avoid every time being the same after reset
     
     // Set up the pattern
@@ -109,26 +131,40 @@ void setup() {
 }
 
 void loop() {
-    if(p1Pattern){
-        showPattern(p1Max);
-        p1Pattern = false;
+    unsigned int currentTime = millis();
+
+    showPatterns(currentTime);
+    updateKeys();
+
+    if(!p1Pattern && keys[KeysP1[pattern[p1Cur]]] == HIGH){
+        p1Cur++;
+        if(p1Cur == p1Max){
+            scoreP1++;
+            p1Cur = 0;
+            p1Max++;
+            if(p1Max == PATTERN_LENGTH) p1Max = 1;
+            p1Pattern = true;
+            updateScreens();
+        }
     }
-    else{
-        updateKeys();
-        if(keys[KeysP1[pattern[p1Cur]]] == HIGH){
-            p1Cur++;
-            if(p1Cur == p1Max){
-                p1Cur = 0;
-                p1Max++;
-                p1Pattern = true;
-            }
+    if(!p2Pattern && keys[KeysP2[pattern[p2Cur]]] == HIGH){
+        p2Cur++;
+        if(p2Cur == p2Max){
+            scoreP2++;
+            p2Cur = 0;
+            p2Max++;
+            if(p2Max == PATTERN_LENGTH) p2Max = 1;
+            p2Pattern = true;
+            updateScreens();
         }
     }
     delay(10);
 }
 
-// put function definitions here:
-//reads the state of the matrix and puts it into keys[]
+// function definitions:
+
+//reads the state of the matrix and puts it into array keys,
+// keys[i] represents whether or not i:th key is pressed
 void updateKeys(){
     for(int i=0; i < size; i++){
         for(int k=0; k < size; k++){
@@ -154,25 +190,64 @@ void updateKeys(){
 //updates the screens to show the points of player 1 and 2
 void updateScreens(){
   lcd1.clear();
-  //lcd1.setCursor(0,0);
   lcd2.clear();
-  //lcd2.setCursor(0,0);
   lcd1.print("Player 1: " + String(scoreP1));
   lcd2.print("Player 2: " + String(scoreP2));
 }
 
+// Returns random free pin between min and max, that's not current
 int randomPin(int current, int min, int max){
     int i = random(min, max);
     while(i == current) i = random(min, max);
     return i;
 }
 
-void showPattern(int until){
-    for(int i = 0; i < until; i++){
-        leds[LedP1[pattern[i]]] = CRGB::Red;
+// TODO - ratkaise ensimmäisen indeksin ongelma :D
+void showPatterns(unsigned long currentTime){
+    // Check if in pattern phase and interval has passed
+    bool p1Next = ((unsigned long)(currentTime - prevTimeP1P) >= PATTERN_INTERVAL);
+    bool p2Next = ((unsigned long)(currentTime - prevTimeP2P) >= PATTERN_INTERVAL);
+
+    if(p1Pattern && p1Next){
+        Serial.print(pattern[indexP1P]);
+        if(p1PStart){
+            p1PStart = false;
+            leds[LedP1[pattern[indexP1P]]] = CRGB::Red;
+        }
+        else{
+            // Set the previous LED in the pattern to off
+            leds[LedP1[pattern[indexP1P]]] = CRGB::Black;
+            indexP1P++;
+            // Check if at the max index
+            if(indexP1P == p1Max){
+                p1Pattern = false; // End of pattern
+                p1PStart = true; // Start flag for next iteration
+                indexP1P = 0;
+                Serial.println();
+            }    
+            else leds[LedP1[pattern[indexP1P]]] = CRGB::Red; // Next in pattern
+        }
+        prevTimeP1P = currentTime; // Time check
         FastLED.show();
-        delay(500);
-        leds[LedP1[pattern[i]]] = CRGB::Black;
+    }
+
+    if(p2Pattern && p2Next){
+        Serial.print(pattern[indexP2P]);
+        if(p2PStart){
+            p2PStart = false;
+            leds[LedP2[pattern[indexP2P]]] = CRGB::Red;
+        }
+        else{
+            leds[LedP2[pattern[indexP2P++]]] = CRGB::Black;
+            if(indexP2P == p2Max){
+                p2Pattern = false;
+                p2PStart = true;
+                indexP2P = 0;
+                Serial.println();
+            }
+            else leds[LedP2[pattern[indexP2P]]] = CRGB::Red;
+        }
+        prevTimeP2P = currentTime;
         FastLED.show();
     }
 }
